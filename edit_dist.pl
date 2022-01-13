09e152f2602183d7b9f1f3befd408638a64d5e15
Matthew Villarica <53604004+antimatter07@users.noreply.github.com>
2022-01-14 00:03:18 +0800

    Displaying of edits MIGHT be working
    test it more

--- edit_dist.pl
+++ edit_dist.pl
@@ -14,38 +14,31 @@ start :- write('Please enter the length of the first string:'),read(X),
          c(X,Y,C,Edits),write('Minimum edit ditance cost: '), write(C), nl,
          write('Edits done: '),write(Edits).
 
-c(0,0,Res,Edits) :- Res=0,(string(Edits) -> string_concat(Edits, "replace",Edits2),Edits=Edits2;Edits = "Replace").
+c(0,0,Res,Edits) :- Res=0, Edits = " ".
 % must insert M items
-c(0,M,Res,Edits) :- M > 0, Res=M,(string(Edits)->string_concat(Edits, "insert",Edits2),Edits=Edits2 ;Edits="Insert").
+c(0,M,Res,Edits) :- M > 0, Res=M,Edits = " ".
 % must delete N items
-c(N,0,N,Edits) :- N > 0,(string(Edits)->string_concat(Edits, "insert",Edits2),Edits=Edits2 ;Edits="Insert").
+c(N,0,N,Edits) :- N > 0, Edits = " ".
 c(N,M,C,Edits) :- N > 0, M > 0,
 
         N1 is N-1, M1 is M-1,
 
-        c(N1,M,C1,Edits1), C1a is C1+1, write(Edits1), nl,      % insert into A
-        c(N,M1,C2,Edits2), C2a is C2+1,    % delete from B
+        c(N1,M,C1,Edits1), C1a is C1+1, atom_concat(Edits1,"Delete!",Edits10),Edits100=Edits10,      % insert into A
+        c(N,M1,C2,Edits2), C2a is C2+1,atom_concat(Edits2,"Insert!",Edits20),Edits200=Edits20,   % delete from B
         c(N1,M1,C3,Edits3),                 % replace
          % if the variable B is equal to A,it means that they are currenty the same (COPY, no
          % cost) OR it has to be replaced (which has a cost of 1).
                 a(N,A), b(M,B), (A==B
-                                -> C3a=C3; C3a is C3+1),
-                min(C1a,C2a,Cm1,Edits1,Edits2,Editscm1), min(Cm1,C3a,C,Editscm1,Edits3,Edits).       % take best of 3 ways
+                                -> C3a=C3, atom_concat(Edits3, "Copy!",Edits30),Edits300=Edits30;
+                                   C3a is C3+1, atom_concat(Edits3, "Replace !",Edits30),Edits300=Edits30),
+                min(C1a,C2a,Cm1,Edits100,Edits200,Editscm1), min(Cm1,C3a,C,Editscm1,Edits300,Edits).       % take best of 3 ways
 
 % if X is less than or equal to Y, then MIN is X. Otherwise, MIN is Y.
 min(X,Y,Z,EditsX,EditsY,EditsZ) :- X =< Y -> Z=X,EditsZ=EditsX ; Z=Y,EditsZ=EditsY.
 
 % example data
-a(1,m). a(2,a). a(3,t).
-b(1,m). b(2,a).
-% to find min, c(7,7,Cost),
+a(1,a). a(2,b). a(3,c). a(4,d). a(5,e).
 
+b(1,a). b(2,z).  b(3,c). b(4,e). b(5,e). b(6,d). b(7,a).
 
-% example data
-a(1,b). a(2,a). a(3,d).
-b(1,b). b(2,c).
-% to find min, c(7,7,Cost),
-
-a(1,b). a(2,a).
-b(1,d). b(2,c).
 % to find min, c(7,7,Cost),
